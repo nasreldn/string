281ffe399c9c7b9ce81eef8c4b684ff709dbcce7
Nasor <nasrsalih99@gmail.com>
2025-07-14 02:08:44 +0300

    first

---
+++ hello.py
@@ -0,0 +1,100 @@
+# myvariable = ' nasr6elden'
+# age = 43
+# print(myvariable +" "+str(age))
+#
+# a = "dnrjoedjoediuri n3id o dib  din"
+# b = len(a)
+# a = "hello"
+# print(a[0:3])
+# a = "python"
+# print(a[::-1])
+# a = "cat"
+# print(a.rjust(5,"0"))
+# # a = "    wow    "
+# # print(a.strip())
+# # a = "b,c,d"
+# # print(a.split(","))
+# # a = "i love python"
+# # print(a.replace("i love python", "i love php"))
+# # a = "my name is nasr"
+# # # print(a.find("k")) 
+# # a = [1,2,4]
+# # b = [5,6,8,9]
+# # for 0
+# # import _sqlite3
+# # conn = sqlite3.connect("students.db")
+# # cursor = (conn.cursor)
+# # cursor.execute"""
+# a = "nasreldin Altayeb"
+# b = a.upper()
+# print(b)
+# c = a.title()
+# # print(c)
+# # d = a.lower()
+# # print(d)
+# # f = a.capitalize()
+# # print(f)
+# # one = ["nasr","kelde","nkmoh","amedk","salih"]
+# # # tow = one.split("k",2)
+# # # print(tow)
+
+
+# # print("".join(one))
+# a = "     gigo    "
+# print(a.strip())
+# print(a.ljust(20))
+# print(a.rjust(30,"s"))
+# b ="nasrelden"
+# print(b[0])
+# print(b[0:4])
+# print(b[-1])
+# print(b[::-1])
+# print(b.count("n",0,1))
+# print(b.find("l",0,3))
+# print(b.startswith("nas"))
+# print(b.endswith("f"))
+# print(b.replace("nasr","fakh"))
+c = [1100,7,30,"a","b","c",True,2.3]
+# 
+# c.insert(3,"z")
+
+# print(c)
+# c.pop(0)
+# print(c)
+
+# print(c)
+# print(c)
+# print(len(c))
+# d=c.copy()
+# d.append("nasr")
+# print(c)
+# print(d)
+# print(c.index("a"))
+# c.sort()
+# print(c)
+# g =[8,9,0]
+# k = c+g
+# print(k)
+
+# b = [6,7,8,9]
+# a.insert(3,"ahmed")
+# b.sort()
+# print(b)
+# a.append(b)
+# print(a)
+# aa = (1,2,3,4,5)
+# bb = list(aa)
+# print(aa.index(3))
+# print(aa[1])
+# print(bb)
+a = {1,2,3,4,5,6}
+b = {4,5,6,7,8}
+print(a|b)#
+print(a&b)#
+print(b-a)#deffrence
+print(a^b)#seymmetric_differnce
+
+
+
+
+
