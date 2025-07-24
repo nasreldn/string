eac0794ac93bb2b06fc7162d563c52bff058fc56
Nasor <nasrsalih99@gmail.com>
2025-07-24 12:41:05 +0300

    filterlambda

--- one/filter.py
+++ one/filter.py
@@ -1,7 +1,10 @@
 def a(nubmer) :
-    return nubmer>5
+    return nubmer > 5
 b = [1,2,3,4,5,6,7,8,9,10]
 c = filter(a,b)
 for d in c :
- 
     print(d)
+print("#"*30) 
+e = ["a","b","c","d","bkld","bknqn"] 
+for f in filter(lambda taxt :taxt.startswith("b"),e):
+    print(f)  
