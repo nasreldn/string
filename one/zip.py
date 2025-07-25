f8ad976ad756f8cd602a092126a1ae8effe58bc0
Nasor <nasrsalih99@gmail.com>
2025-07-26 01:49:18 +0300

    zip

---
+++ one/zip.py
@@ -0,0 +1,10 @@
+a= [1,2,3,4,5,6,7,8]
+b= ("a","s","f","g","h")
+d= "nasir"
+y={"a":"20","z":"90","gan":"jjj"}
+for s,b,d,f in zip(a,b,d,y):
+    print(s)
+    print(b)
+    print(d)
+    print(f"{f} uyi{y[f]}")
+    
