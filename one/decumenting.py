605c8ee3ffffca17b615b3e9cc4418846c30b30e
Nasor <nasrsalih99@gmail.com>
2025-07-26 19:17:10 +0300

    documenting

---
+++ one/decumenting.py
@@ -0,0 +1,16 @@
+def mydecumenting(name):
+    """this function return "hello{name}"""
+    print(name)
+mydecumenting("nasr")
+print(dir(mydecumenting))
+help(mydecumenting) 
+
+print("#"*50)   
+def a(name):
+    """hello"""
+    
+    print(name)
+a("nasr")
+print(dir(a))
+help(a)
+print(a.__doc__)
