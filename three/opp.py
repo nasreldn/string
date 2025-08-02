a5b7c5639715a684600cd1ce4b441484e02efaf8
Nasor <nasrsalih99@gmail.com>
2025-08-02 21:00:59 +0300

    oop1

---
+++ three/opp.py
@@ -0,0 +1,14 @@
+class name:
+    def __init__(name,fname,mname,lname):
+        name.frname=fname
+        name.mdrname=mname
+        name.lsname=lname
+a= name("nasr","altayeb","abdalla")
+b= name("ali","ebrahim","alkhider")
+c= name("ashraf","aljaile","aljined")
+# print(dir(name))
+# print(dir(str))
+# print(a.__class__)
+print(a.frname,a.mdrname,a.lsname)
+print(b.frname,a.mdrname,a.lsname)
+print(c.frname,a.mdrname,a.lsname)         
