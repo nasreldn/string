91ba53306f2eec2d1101f03948d6e79db6d90cb9
Nasor <nasrsalih99@gmail.com>
2025-08-12 12:23:40 +0300

    group_elements

--- function/one.py
+++ function/one.py
@@ -1,31 +1,43 @@
-def sayhello(name):
-   if type(name)!= str:
-        print("your name must be string")
-   else:
+# def sayhello(name):
+#    if type(name)!= str:
+#         print("your name must be string")
+#    else:
             
-        print(f"hello {name.strip().upper()}")
-a,b,c="nasr","ali","omer"        
-sayhello(a)
-sayhello(b)
-sayhello(c)
-def a(n1,n2):
+#         print(f"hello {name.strip().upper()}")
+# a,b,c="nasr","ali","omer"        
+# sayhello(a)
+# sayhello(b)
+# sayhello(c)
+# def a(n1,n2):
 
     
     
-b =(input("enter your first number: "))
+# b =(input("enter your first number: "))
 
-c =(input("enter your scond number: "))
-a(int(b),int(c)) 
-def b(name,*maharat):
-    print(f"hello {name} goood")
-    for a in maharat:
-        print(a)
-b("nasr","html","css","c","python")
-b("ali","js","css","php","sql")
+# c =(input("enter your scond number: "))
+# a(int(b),int(c)) 
+# def b(name,*maharat):
+#     print(f"hello {name} goood")
+#     for a in maharat:
+#         print(a)
+# b("nasr","html","css","c","python")
+# b("ali","js","css","php","sql")
          
-def h(name,age,contary="unknown"):
-    print(f"hello {name} your age is {age} and your contary is {contary}")
-h("ali",5686) 
-h("nasr","25","sudan")
-def g()  
-           
+# def h(name,age,contary="unknown"):
+#     print(f"hello {name} your age is {age} and your contary is {contary}")
+# h("ali",5686) 
+# h("nasr","25","sudan")
+a= [[1,2,3],[4,5,6],[7,8,9]]
+b=[]
+c=0
+for d in range(len(a[0])):
+    b.append([])
+    for e in range(len(a)):
+        b[c].append(a[e][c])
+    c +=1    
+f,g,h=b[0],b[1],b[2]
+print(f,g,h)        
+
+       
+    
+
--- string/dictionary.py
+++ string/dictionary.py
@@ -1,21 +1,29 @@
-a = {"name":"nasr",
-     "age":25,
-     }
-a["contary"]="sudan"
-print(a)
-a.update({"nnn":"nidhidr"})
-print(a)
-b=a.copy()
-print(b)
-a.update({"ihiehe":"3333"})
-print(a.keys())
-print(a.values())
-# print(a.clear())
+# a = {"name":"nasr",
+#      "age":25,
+#      }
+# a["contary"]="sudan"
+# print(a)
+# a.update({"nnn":"nidhidr"})
+# print(a)
+# b=a.copy()
+# print(b)
+# a.update({"ihiehe":"3333"})
+# print(a.keys())
+# print(a.values())
+# # print(a.clear())
 
-print(a.setdefault("ioo","bebo"))
-print(a.popitem())
-c =  a.items()
-print(c)
-z =("ejdeji","jeieidjjk","j4jd")
-q =("12")
-print(dict.fromkeys(z,q))
+# print(a.setdefault("ioo","bebo"))
+# print(a.popitem())
+# c =  a.items()
+# print(c)
+# z =("ejdeji","jeieidjjk","j4jd")
+# q =("12")
+# print(dict.fromkeys(z,q))
+a =[1,24,4,5,7,9,6]
+b = [] 
+for c in a:
+     if c > a[0]:
+          print(f"{c}")
+     else:
+         print("hei")
+# print(f"{c}")          
