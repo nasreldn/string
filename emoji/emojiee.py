5ef00848fa4371c9cdbbb3b44e6a9ffdc5ee40bd
Nasor <nasrsalih99@gmail.com>
2025-08-14 09:16:56 +0300

    emojee

---
+++ emoji/emojiee.py
@@ -0,0 +1,5 @@
+import emoji
+print(emoji.emojize("i love reading books:blue_heart:"))
+print(emoji.emojize("nasr is :face_with_monocle:",language="alias"))
+print(emoji.emojize("nasr is :lion:",language="alias"))
+print(emoji.emojize("people huugging:people_hugging:",language="alias"))
