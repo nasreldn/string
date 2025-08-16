8e45eadf7f0202f7161c59f006c50b99f293dad5
Nasor <nasrsalih99@gmail.com>
2025-08-16 10:02:57 +0300

    recipe

---
+++ oop/recpe_collaction.py
@@ -0,0 +1,8 @@
+print("welcome to the recipe collection\n")
+
+a=input("enter recipe name: ")
+b=input("enter ingredients: ")
+c=input("enter recipe instructions: ")
+d=input("enter cooking time: ")
+
+print(f"recipe name:{a}\n ingredients:{b}\n instructions:{c}\n cooking time:{d}")
